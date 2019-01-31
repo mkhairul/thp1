4557b0502fe05ab89b89d6b5ab0b79bcabb47222
Khairul <mkhairul@gmail.com>
2019-01-31 20:23:49 +0800

    removed filter test

--- tests/Feature/PropertyTest.php
+++ tests/Feature/PropertyTest.php
@@ -19,18 +19,4 @@ class PropertyTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function testPropertyFilter()
-    {
-        /* 
-        $headers = [];
-        $headers['CONTENT_TYPE'] = 'application/json';
-        $headers['Accept'] = 'application/json';
-
-        $server = $this->transformHeadersToServerVars($headers);
-
-        $response = $this->call('POST', 'api/property', [], [], [], $server, json_encode(['filters' => [ 'state' => 'KL' ]]));
-
-        $response->assertStatus(200); */
-        $this->assertTrue(true);
-    }
 }
