fe15f33e3e2da810b206fa4cca993a35f534f0e1
Khairul <mkhairul@gmail.com>
2019-01-31 20:40:35 +0800

    removed user seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        factory(App\User::class, 50)->create();
         factory(App\Property::class, 100)->create();
     }
 }
